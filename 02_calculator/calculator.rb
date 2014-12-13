c7f338a4711dce5069de630df5b1cb4424a46d95
andylinteau <andy.linteau@gmail.com>
2014-12-13 18:29:59 -0500

    add tests for calculator and make tests pass

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -16,5 +16,46 @@ def sum(numbers)
 		sum = sum + number
 	end
 	return sum.to_i
+end
+
+def multiply(x,y)
+	product = x * y
+end
+
+def multiply_numbers(numbers)
+	first_number = numbers[0]
+	numbers.each do |number|
+	first_number = first_number * number
+	end
+	first_number
+end
+
+def power(x,exponent)
+	number = x
+	if exponent <= 2
+		number = x*x
+		
+	elsif exponent <= 3
+		(exponent - 1).times do
+			number = number*x
+		end
+	end		
+
+	return number
+
+end
+
+def factorial(n)
+	factorial = 0
+	result = n
+	if n == 0 || n == 1
+		factorial = 1
+	elsif n >= 2
+		while n > 1
+		result = result*(n-1)
+		n = n - 1
+		end
+		return result
+	end
 
 end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,21 +79,49 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    multiply(1,3).should == 3
+    multiply(0,3).should == 0
+    multiply(3,3).should == 9
+  end
 
-  it "multiplies several numbers"
+  it "multiplies several numbers" do
+    multiply_numbers([1,1,1]).should == 1
+    multiply_numbers([1,2,3]).should == 6
+    multiply_numbers([3,3,3]).should == 81
+  end
 
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+    power(1,3).should == 1
+    power(2,3).should == 8
+    power(8,3).should == 512
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  
+  it "computes the factorial of 0" do 
+    factorial(0).should == 1
+  end
+
+  it "computes the factorial of 1" do
+    factorial(1).should == 1
+  end
+
+  it "computes the factorial of 2" do
+    factorial(2).should == 2
+  end
+
+  it "computes the factorial of 5" do
+    factorial(5).should == 120
+  end
+
+  it "computes the factorial of 10" do
+    factorial(10).should == 3628800
+  end
+
 end

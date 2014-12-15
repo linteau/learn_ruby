b31feca31a2eeed9db771b02acf04582065c0e52
andylinteau <andy.linteau@gmail.com>
2014-12-14 23:01:02 -0500

    finish translate function for pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -7,7 +7,8 @@ def translate(word)
 			first_letter = letters[0]
 			letters.delete(letters[0])
 			letters.join("") + first_letter + "ay"
-			elsif letters[0].include?("a") || letters[0].include?("e") || letters[0].include?("i") || letters[0].include?("o") || letters[0].include?("u") == false && letters[1].include?("a") || letters[1].include?("e") || letters[1].include?("i") || letters[1].include?("o") || letters[1].include?("u") == false && letters[2].include?("a") || letters[2].include?("e") || letters[2].include?("i") || letters[2].include?("o") || letters[2].include?("u") == true
+		elsif letters[0].include?("a") || letters[0].include?("e") || letters[0].include?("i") || letters[0].include?("o") || letters[0].include?("u") == false && letters[1].include?("a") || letters[1].include?("e") || letters[1].include?("i") || letters[1].include?("o") || letters[1].include?("u") == false && letters[2].include?("a") || letters[2].include?("e") || letters[2].include?("i") || letters[2].include?("o") || letters[2].include?("u") == true && letters[1].include?("q") == false && letters[2].include?("u") == false
+			puts "i just went"
 			first_two_letter = letters[0] + letters[1]
 			letters.delete(letters[0])
 			letters.delete(letters[0])
@@ -18,6 +19,19 @@ def translate(word)
 			letters.delete(letters[0])
 			letters.delete(letters[0])
 			letters.join("") + first_three_letter + "ay"
+		elsif letters[0].include?("a" || "e" || "i" || "o" || "u") == false && letters[1].include?("q") == true && letters[2].include?("u") == true
+			puts "I am here now"
+			puts first_three_letter = letters[0] + letters[1] + letters[2]
+			letters.delete(letters[0])
+			letters.delete(letters[0])
+			letters.delete(letters[0])
+			letters.join("") + first_three_letter + "ay"
+		elsif letters[0].include?("q") == true && letters[1].include?("u") == true
+			puts "i just went"
+			first_two_letter = letters[0] + letters[1]
+			letters.delete(letters[0])
+			letters.delete(letters[0])
+			letters.join("") + first_two_letter + "ay"
 		end
 	elsif word.include?(" ") == true
 		puts "i have a space"
@@ -28,6 +42,12 @@ def translate(word)
 			if letters[0].include?("a") || letters[0].include?("e") || letters[0].include?("i") || letters[0].include?("o") || letters[0].include?("u")
 				puts "me"
 				pig_latin_word.push(word + "ay")
+			elsif letters[0].include?("q") == true && letters[1].include?("u") == true
+			puts "i just went"
+			first_two_letter = letters[0] + letters[1]
+			letters.delete(letters[0])
+			letters.delete(letters[0])
+			pig_latin_word.push(letters.join("") + first_two_letter + "ay")
 			elsif letters[0].include?("a") || letters[0].include?("e") || letters[0].include?("i") || letters[0].include?("o") || letters[0].include?("u") == false && letters[1].include?("a") || letters[1].include?("e") || letters[1].include?("i") || letters[1].include?("o") || letters[1].include?("u") == true 
 				first_letter = letters[0]
 				letters.delete(letters[0])
@@ -38,7 +58,6 @@ def translate(word)
 				letters.delete(letters[0])
 				pig_latin_word.push(letters.join("") + first_two_letter + "ay")
 			end
-
 		end
 		return pig_latin_word.join(" ")
 	end
